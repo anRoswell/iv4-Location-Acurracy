0b25b48abc170303b730244bff3130093eac6af2
Alfonso Navarro <alfonso.navarro@syspotec.com>
2019-10-08 09:32:40 -0500

    Caso de uso plugin location acurracy ionic v4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { LocationAccuracy } from '@ionic-native/location-accuracy/ngx';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -16,6 +17,7 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    LocationAccuracy,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,81 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { LocationAccuracy } from "@ionic-native/location-accuracy/ngx";
+import { Platform } from '@ionic/angular';
+
+declare let cordova: any;
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+	selector: "app-home",
+	templateUrl: "home.page.html",
+	styleUrls: ["home.page.scss"],
 })
 export class HomePage {
+  constructor(
+    private locationAccuracy: LocationAccuracy,
+    private platform : Platform
+    ) {}
 
-  constructor() {}
+	ngOnInit() {
+    this.platform.ready()
+    .then(()=>{
+      cordova.plugins.autoStart.enable();
+		  this.ActivarGps();
+    })
+	}
 
+	ActivarGps() {
+		this.locationAccuracy.canRequest().then((canRequest: boolean) => {
+			if (canRequest) {
+				// the accuracy option will be ignored by iOS
+				this.locationAccuracy
+					.request(this.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY)
+					.then(
+						() => console.log("Request successful"),
+						error =>
+							console.log("Error requesting location permissions", error),
+					);
+			}
+		});
+  }
+  
+  GetPosition(){
+    navigator.geolocation.getCurrentPosition(this.onSuccess, this.onError);
+  }
+
+	// onSuccess Geolocation
+	//
+	onSuccess(position) {
+		var element = document.getElementById("geolocation");
+		element.innerHTML =
+			"Latitude: " +
+			position.coords.latitude +
+			"<br />" +
+			"Longitude: " +
+			position.coords.longitude +
+			"<br />" +
+			"Altitude: " +
+			position.coords.altitude +
+			"<br />" +
+			"Accuracy: " +
+			position.coords.accuracy +
+			"<br />" +
+			"Altitude Accuracy: " +
+			position.coords.altitudeAccuracy +
+			"<br />" +
+			"Heading: " +
+			position.coords.heading +
+			"<br />" +
+			"Speed: " +
+			position.coords.speed +
+			"<br />" +
+			"Timestamp: " +
+			position.timestamp +
+			"<br />";
+	}
+
+	// onError Callback receives a [PositionError](PositionError/positionError.html) object
+	//
+	onError(error) {
+		alert("code: " + error.code + "\n" + "message: " + error.message + "\n");
+	}
 }
